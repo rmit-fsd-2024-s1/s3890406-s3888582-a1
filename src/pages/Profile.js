f7cdf24a07fab334efda565da428f3d4380e9f60
Mike Papadopoulos <141984049+s3888582@users.noreply.github.com>
2024-05-16 12:47:24 +1000

    Update Profile.js
    for css purposes

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -28,28 +28,29 @@ function Profile(username) {
   }
 
   return (
-    <div id = "content">
-      <h2 id = "profile-title">Profile</h2>
+    <div id="content">
+      <h2 id="profile-title">Profile</h2>
 
-      <p>
         {name === null ?
             <>
               <p>You must Login to view the contents of this page.</p>           
             </>
             :
             <>
-            <div id = "user-details">
+            <div id="user-details">
               <p>Name: {name} </p>
               <p>Email: {email} </p>
               <p>Join date: {joindate} </p>
-              
             </div>
+
+            <div id="button-container">
               <button onClick={edit_details} id="edit-details-btn">Edit details</button> <br></br><br></br>
               <button onClick={logout_user} id="logout-btn">Logout</button> <br></br><br></br>
               <button onClick={del_user} id="delete-user-btn">Delete Account</button>
+            </div>
             </>
           }
-      </p>
+
     </div>
   );
 }
